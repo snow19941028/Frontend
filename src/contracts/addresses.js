50e589cceaf45550be93632a9e56b40d9bbb63d3
snow19961028 <p826093@gmail.com>
2024-02-13 13:08:46 +0900

    first commit

---
+++ src/contracts/addresses.js
@@ -0,0 +1,9 @@
+// This address points to a dummy ERC20 contract deployed on Ethereum Mainnet,
+// Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
+const addresses = {
+  ceaErc20: "0x877FE2925774219d8C6649D12d339d8F04c72d0f",
+  birdErc20: "0x877FE2925774219d8C6649D12d339d8F04c72d0f",
+  kovan: "0x873C83291691eE1e8c840dFB463c2c75406f2b1d"
+};
+
+export default addresses;
